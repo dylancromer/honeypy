1aa67779079030f91bb50654d4ebc485c106fd48
DylanC <dylan@dylancromer.com>
2018-07-01 17:17:56 -0400

    Better logging

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ import sys, http.server, socketserver, cgi, logging, time
 
 currenttime = time.strftime("%Y-%m-%d.%H-%M-%S.%Z", time.localtime())
 logging.basicConfig(filename='http_' + currenttime + '.log',level=logging.DEBUG)
+sys.stderr = open('shttph_' + currenttime + '.log', 'w', buffer)
 
 print("""
   _  _                   ___
